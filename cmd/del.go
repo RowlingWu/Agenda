ab693518ac72ee108ab13313e5f9fe19828b5236
Rowling <438375422@qq.com>
2017-10-30 14:11:52 +0800

    try go test

--- cmd/del.go
+++ cmd/del.go
@@ -52,5 +52,4 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// delCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
